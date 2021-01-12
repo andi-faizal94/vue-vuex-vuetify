7483776e4ab087eb9574606e44b3537bd8ff26e9
andi-faizal94 <andifaizal647@gmail.com>
2021-01-12 13:41:29 +0700

    Updates

---
+++ js/backend.bd5c31bb.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["backend"],{f1d0:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div")},l=[],u=t("2877"),a={},r=Object(u["a"])(a,c,l,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=backend.bd5c31bb.js.map
---
+++ js/mobile.0534bbec.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["mobile"],{"9ae2":function(e,n,t){"use strict";t.r(n);var l=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},u=[],a=t("2877"),c={},i=Object(a["a"])(c,l,u,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=mobile.0534bbec.js.map
